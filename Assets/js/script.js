4e89f3926f04ccd729d86f28c198ff8043535afd
Sami2 <coolsweetsami@gmail.com>
2023-04-06 19:28:44 -0400

    fixes to html and js, comments added

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,4 +1,4 @@
-
+//when the user adds to the callender and clicks save it saves to localStorage
 $(document).ready(function () {
   $(".saveBtn").on("click", function() {
     var timeBlockId = $(this).closest(".time-block").attr("id");
@@ -6,32 +6,35 @@ $(document).ready(function () {
     localStorage.setItem(timeBlockId, userInput);
     console.log("click");
   });
-  var currentHour = dayjs().format('HH A');
-  console.log(currentHour);
+  //gets current time in 24hours
+  var currentHour = dayjs().$H;
+//takes the current time and compares it to the block time on schedule
   $(".time-block").each(function() {
-    var blockHour = $(this).attr("id").split("-");
-    console.log(blockHour);
-    if (blockHour <= currentHour) {
-      $(this).addClass("past").removeClass("present future");
+    var blockHour = parseInt($(this).attr("id").split("-")[1]);
+    
+    if (blockHour < currentHour) {
+      $(this).addClass("past");
     }
-    else if (blockHour >= currentHour) {
-      $(this).addClass("future").removeClass("past present");
+    else if (blockHour > currentHour) {
+      $(this).addClass("future");
     }
     else {
-      $(this).addClass("present").removeClass("past future");
+      $(this).addClass("present");
     }
   });
   
+  //this grabs the informations from localStorage
   $(".time-block").each(function() {
     var timeBlockId = $(this).attr("id");
     var savedUserInput = localStorage.getItem(timeBlockId);
     $(this).find(".description").val(savedUserInput);
   });
   
+  //displays the date at the top of the page
   const currentDate = new Date();
-
-  // Format the date as "Month Day, Year"
-  const options = { year: 'numeric', month: 'long', day: 'numeric'};
+  //this gets the date and diplays it in a specific formate
+  
+  const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'};
   const formattedDate = currentDate.toLocaleDateString('en-US', options);
 
   // Set the current date in the header
