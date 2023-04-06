4d8532e2dff83faf381e9929100d1eb3a9da1a5d
Sami2 <coolsweetsami@gmail.com>
2023-04-05 21:01:44 -0400

    updates to js, added time comparision and reading current day on page

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,23 +1,40 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+
+$(document).ready(function () {
+  $(".saveBtn").on("click", function() {
+    var timeBlockId = $(this).closest(".time-block").attr("id");
+    var userInput = $(this).siblings(".description").val();
+    localStorage.setItem(timeBlockId, userInput);
+    console.log("click");
+  });
+  var currentHour = dayjs().format('HH A');
+  console.log(currentHour);
+  $(".time-block").each(function() {
+    var blockHour = $(this).attr("id").split("-");
+    console.log(blockHour);
+    if (blockHour <= currentHour) {
+      $(this).addClass("past").removeClass("present future");
+    }
+    else if (blockHour >= currentHour) {
+      $(this).addClass("future").removeClass("past present");
+    }
+    else {
+      $(this).addClass("present").removeClass("past future");
+    }
+  });
+  
+  $(".time-block").each(function() {
+    var timeBlockId = $(this).attr("id");
+    var savedUserInput = localStorage.getItem(timeBlockId);
+    $(this).find(".description").val(savedUserInput);
+  });
+  
+  const currentDate = new Date();
+
+  // Format the date as "Month Day, Year"
+  const options = { year: 'numeric', month: 'long', day: 'numeric'};
+  const formattedDate = currentDate.toLocaleDateString('en-US', options);
+
+  // Set the current date in the header
+  document.getElementById('currentDay').textContent = formattedDate;
+
 });
